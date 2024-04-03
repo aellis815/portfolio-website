1db31e386e0fbecdee82b8d25b7abeb552358eef
aellis815 <alexander.ellis815@gmail.com>
2024-04-03 10:54:41 -0700

    added some styling - don't love it

--- src/app/component/HeroArea.tsx
+++ src/app/component/HeroArea.tsx
@@ -1,28 +1,61 @@
+//Needs to be client side due to type animation
+'use client';
 import React from 'react'
 import Image from 'next/image'
+import { TypeAnimation } from 'react-type-animation'
 
-const HeroArea = () => {
+export default function HeroArea() {
     return (
         <section>
-            <div className="grid grid-cols-1 lg:grid-cols-12">
-                <div className="col-span-7 place-self-center">
-                    <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">Hey, I'm Alex</h1>
-                    <p className="text-[#ADB7BE] text-lg lg:text-xl">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum
+            <div className="grid grid-cols-1 sm:grid-cols-12">
+                <div className="col-span-7 place-self-center text-center sm:text-left">
+                    <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+                        <span className="text-transparent bg-clip-text bg-gradient-to-br from-[#0B666A] to-[#97FEED]">Hey, I'm{" "}
+                        </span>
+                        {/* Using Type Animation from react-type animation to animate intro text */}
+                        <br></br>
+                        <TypeAnimation
+                            sequence={[
+                                "Alex",
+                                1000,
+                                "an Aspiring Developer",
+                                1000,
+                            ]}
+                            wrapper="span"
+                            speed={13}
+                            repeat={Infinity}
+                        />
+                    </h1>
+                    <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+                        I love building things for the web. I specialize in creating fast, responsive, and accessible websites.
                     </p>
-                </div>
-                <div>
-                    <button className="px-6 py-3 rounded-full mt-3 bg-white hover:bg-slate-200 text-black mr-4">Contact Me</button>
-                    <button className="px-6 py-3 rounded-full mt-3 bg-transparent hover:bg-slate-800 text-white border border-white">Download Resume</button>
+
+                    <div>
+                        <button className="
+                            px-6 
+                            py-3 
+                            w-full 
+                            sm:w-fit 
+                            rounded-full 
+                            mt-3 
+                            bg-gradient-to-br from-[#97FEED] via-[#0B666A] to-[#35A29F] hover:bg-white text-white mr-4">
+                            Contact Me
+                        </button>
+                        <button className="px-1 py-1 w-full sm:w-fit rounded-full mt-3 bg-gradient-to-br from-[#97FEED] via-[#0B666A] to-#35A29F hover:bg-slate-800 text-white">
+                            <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                                Download CV
+                            </span>
+                        </button>
+                    </div>
                 </div>
                 <div className="col-span-5 place-self-center mt-4 lg:mt-0">
-                    <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[300px] h-[400px] relative">
+                    <div className="rounded-full bg-gradient-to-br from-[#97FEED] via-[#0B666A] to-[#35A29F] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
                         <Image
                             src="/images/circle_headshot.png"
                             className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                             alt="alex's face"
-                            width={300}
-                            height={300}
+                            width={600}
+                            height={600}
                         />
                     </div>
                 </div>
@@ -31,4 +64,3 @@ const HeroArea = () => {
     )
 }
 
-export default HeroArea;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,10 @@ import HeroArea from "./component/HeroArea";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto py-4 px-12">
-      <HeroArea />
+    <main className="flex min-h-screen flex-col bg-[#071952]">
+      <div className="container mx-auto py-4 px-12">
+        <HeroArea />
+      </div>
     </main>
   );
 }
