4f5e22187b374c9d62d4f067a9642642c362daaf
aellis815 <alexander.ellis815@gmail.com>
2024-04-21 17:20:55 -0700

    i promise to stop tinkering for now. Added smoot scrolling as a finishing touch for now (unless bugs are surfaced to me)

--- src/app/component/NavLink.tsx
+++ src/app/component/NavLink.tsx
@@ -1,25 +1,41 @@
 import Link from 'next/link'
-import { FC, MouseEventHandler } from 'react'
+import { FC, MouseEvent } from 'react'
 
 interface NavLinkProps {
     href: string
     title: string
 }
 
-const NavLink: FC<NavLinkProps> = ({ href, title}) => {
-    return (
+const NavLink: FC<NavLinkProps> = ({ href, title }) => {
+    //event handler for smoothly scrolling to the target element
+    const handleScroll = (e: MouseEvent<HTMLAnchorElement>) => {
+        //stop the default behavior of the tag (might be unncessary for NextJs LINK)
+        e.preventDefault();
 
-        <Link
-            href={href}
-            className="block py-3 pl-3 pr-3 text-slate-300 sm:text-xl rounded md:p-0 hover:text-white"
-            
+        
+        const href = e.currentTarget.href;
+        if (href.includes('#')) {
+            const targetId = href.replace(/.*\#/, '');
+            const elem = document.getElementById(targetId);
+            elem?.scrollIntoView({ behavior: 'smooth' });
+        }  else {
+            // If href doesn't contain '#', navigate to the page normally
+            window.scrollTo({ top: 0, behavior: 'smooth' })
+        }
+    };
+        return (
+
+            <Link
+                href={href}
+                className="block py-3 pl-3 pr-3 text-slate-300 sm:text-xl rounded md:p-0 hover:text-white"
+                onClick={handleScroll}
             >
                 {title}
-            
-        </Link >
 
-    );
-};
+            </Link >
 
-export default NavLink;
+        );
+    };
+
+    export default NavLink;
 
